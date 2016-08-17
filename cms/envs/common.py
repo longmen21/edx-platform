e9a0b2253d757417d8195fddb1edc4372aa629cc
Peter Desjardins <pdesjardins@edx.org>
2016-08-17 16:51:36 -0400

    Creating a DOC_LINK_BASE_URL configuration property and setting its default value to False.

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -1199,4 +1199,4 @@ AFFILIATE_COOKIE_NAME = 'affiliate_id'
 
 ############## Settings for Studio Context Sensitive Help ##############
 
-USE_EDX_PARTNER_DOCUMENTATION = False
+DOC_LINK_BASE_URL = False
--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2953,4 +2953,4 @@ REDIRECT_CACHE_KEY_PREFIX = 'redirects'
 
 ############## Settings for LMS Context Sensitive Help ##############
 
-USE_EDX_PARTNER_DOCUMENTATION = False
+DOC_LINK_BASE_URL = False
